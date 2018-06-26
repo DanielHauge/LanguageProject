dcb2526727472b25b36f67e05ffc6d57e6b84a81
DanielHauge <Animcuil@gmail.com>
2018-06-26 20:05:41 +0200

    improvements to C#

---
+++ C#/Problems/Problems/Experimenter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Problems
+{
+    class Experimenter
+    {
+
+        public void RunExperiment(Problem p)
+        {
+            Console.WriteLine("Running experiments on: "+ p.GetType().Name);
+            List<double> meassurements = new List<double>();
+            bool res;
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < 100000; i++)
+            {
+                sw.Reset();
+                sw.Start();
+                res = p.Calculate();
+                sw.Stop();
+                double ticks = sw.ElapsedTicks;
+                meassurements.Add((ticks / Stopwatch.Frequency)* 1000000000);
+                if (!res)
+                {
+                    Console.WriteLine("Uhh the Algorithm failed");
+                }
+            }
+
+            double[] results = meassurements.ToArray();
+            Console.WriteLine("Median: "+Math.Round(Median(results))+"ns");
+            Console.WriteLine("Average: "+Math.Round(Average(results))+"ns");
+            Console.WriteLine();
+
+        }
+
+        private static double Median(double[] durs)
+        {
+            Array.Sort(durs);
+            return durs[durs.Length / 2];
+        }
+
+        private static double Average(double[] durs)
+        {
+            double total = 0;
+            foreach (double d in durs)
+            {
+                total += d;
+            }
+            return total / durs.Length;
+        }
+
+
+    }
+}
---
+++ C#/Problems/Problems/Problem1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems
+{
+    class Problem1 : Problem
+    {
+
+        int[] list;
+        int k;
+        bool exp;
+
+        public Problem1(int[] list, int k, bool exp)
+        {
+            this.list = list;
+            this.k = k;
+            this.exp = exp;
+        }
+
+        public bool Calculate()
+        {
+            bool res = false;
+            HashSet<int> hashSet = new HashSet<int>();
+            foreach (int i in list)
+            {
+                if (hashSet.Contains(i))
+                {
+                    res = true;
+                }
+                else
+                {
+                    hashSet.Add(k - i);
+                }
+            }
+
+
+            return res;
+        }
+
+
+    }
+}
