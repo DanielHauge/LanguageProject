5cc7b8558f6eea27cef26338b2e2b6e0706e09d9
DanielHauge <Animcuil@gmail.com>
2018-07-08 22:58:22 +0200

    Javascript added

---
+++ Javascript/Problem/Problem/js/Experimenter.js
@@ -0,0 +1,33 @@
+﻿class Experimenter {
+    RunExperiment(problem) {
+        var measurements = [];
+        var res = false;
+        var i;
+        for (i = 0; i < 100; i++) {
+            var start = performance.now();
+            var tempres = problem.Calculate();
+            var dur = performance.now() - start;
+            measurements.push(dur);
+            if (tempres != problem.exp) {
+                console.log("Something went wrong!");
+            }
+        }
+        var resultString = "\nRunning Experiment on: " + problem.constructor.name + "\nMedian: " + String(this.Median(measurements) * 1000000) + "ns \nAverage: " + String(this.Average(measurements) * 1000000)+"ns";
+        return resultString;
+
+
+    }
+
+    Average(numbers) {
+        var total = 0;
+        numbers.forEach(function (element) {
+            total += element;
+        })
+        return total / numbers.length;
+    }
+
+    Median(numbers) {
+        numbers.sort();
+        return numbers[numbers.length / 2];
+    }
+}
---
+++ Javascript/Problem/Problem/js/Problem1.js
@@ -0,0 +1,18 @@
+﻿function Problem1(list, k, exp) {
+    this.list = list;
+    this.k = k;
+    this.exp = exp;
+}
+
+Problem1.prototype.Calculate = function () {
+    var res = false;
+    const set = new Set([]);
+    this.list.forEach(function (element) {
+        if (set.has(element)) {
+            res = true;
+            return res;
+        }
+        set.add(this.k - element);
+    });
+    return res;
+}
